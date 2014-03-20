640c6e848172d6fe053da6f3ea7685c0a0c4a889
Randall Degges <r@rdegges.com>
2014-03-20 12:24:25 -0700

    Removing 2.6 support.

--- setup.py
+++ setup.py
@@ -63,7 +63,6 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
