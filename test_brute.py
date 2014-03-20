fcf355c89d5ccc089b4cf99c447fa461049db8ec
Randall Degges <r@rdegges.com>
2014-03-20 11:34:34 -0700

    Adding test for default characters.

--- test_brute.py
+++ test_brute.py
@@ -18,3 +18,24 @@ class TestBrute(TestCase):
 
     def test_brute_returns_generator(self):
         self.assertIsInstance(brute(), GeneratorType)
+
+    def test_letters_numbers_symbols_whitespace_default(self):
+        letters = False
+        numbers = False
+        symbols = False
+        whitespace = True
+
+        for pw in brute():
+            if 'a' in pw:
+                letters = True
+            elif '1' in pw:
+                numbers = True
+            elif '!' in pw:
+                symbols = True
+            elif ' ' in pw:
+                whitespace = False
+
+        self.assertTrue(letters)
+        self.assertTrue(numbers)
+        self.assertTrue(symbols)
+        self.assertTrue(whitespace)
