4130122937a06e6931132413ea78445414a8dd85
Randall Degges <r@rdegges.com>
2014-03-20 10:24:02 -0700

    Adding a setup.py script.

---
+++ setup.py
@@ -0,0 +1,59 @@
+"""Packaging information."""
+
+
+from setuptools import setup
+
+from brute import (
+    __doc__ as description,
+    __version__ as version,
+)
+
+
+setup(
+
+    # Basic package information:
+    name = 'brute',
+    version = version,
+    py_modules = ('flask_rqify', ),
+
+    # Packaging options:
+    zip_safe = False,
+    include_package_data = True,
+
+    # Package dependencies:
+    install_requires = [
+        'Flask-RQ==0.2',
+        'Flask-Script>=0.5.3',
+    ],
+
+    # Metadata for PyPI:
+    author = 'Randall Degges',
+    author_email = 'r@rdegges.com',
+    license = 'UNLICENSE',
+    url = 'https://github.com/rdegges/brute',
+    keywords = 'python security bruteforce hash hack',
+    description = 'Simple brute forcing in Python.',
+    long_description = description,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Security',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+)
